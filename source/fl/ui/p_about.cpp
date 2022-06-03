feb6b48b4f8a50aafc053e649648dd5324481f40
Ontwikseltsaar <20740429+Ontwikseltsaar@users.noreply.github.com>
2022-06-03 16:02:25 +0200

    Update about page.

--- source/fl/ui/p_about.cpp
+++ source/fl/ui/p_about.cpp
@@ -2,6 +2,6 @@
 
 void fl::ui::about::update(PracticeUI &ui) {
 	ui.printf("Welcome to the Practice Mod!\n");
-	ui.printf("Made by Fruityloops#8500 and contributors\n");
+	ui.printf("Created by Fruityloops#8500 and contributors.\n");
 	ui.printf("Big thanks to CraftyBoss, bryce_____ and Mars2030!\n");
 }
