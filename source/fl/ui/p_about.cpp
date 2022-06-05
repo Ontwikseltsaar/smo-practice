25ea6652856dd81369a1314463c28f1add9f86a3
Ontwikseltsaar <20740429+Ontwikseltsaar@users.noreply.github.com>
2022-06-05 23:25:47 +0200

    Various wording changes.

--- source/fl/ui/p_about.cpp
+++ source/fl/ui/p_about.cpp
@@ -1,7 +1,9 @@
 #include "fl/ui/pages.h"
 
 void fl::ui::about::update(PracticeUI &ui) {
-	ui.printf("Welcome to the Practice Mod!\n");
+	ui.printf("Welcome to the Practice Mod!\n\n");
+	ui.printf("Press L + left D-pad to hide/unhide the menu.\n");
+	ui.printf("Press L + right D-pad to disable/enable the menu.\n\n");
 	ui.printf("Created by Fruityloops#8500 and contributors.\n");
 	ui.printf("Big thanks to CraftyBoss, bryce_____ and Mars2030!\n");
 }
