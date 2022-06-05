1fc2836bc71cc86d130893b11ced2c8fd2eb230e
Ontwikseltsaar <20740429+Ontwikseltsaar@users.noreply.github.com>
2022-06-05 23:27:09 +0200

    Various formatting changes to the codebase.

--- source/fl/ui/p_mooninfo.cpp
+++ source/fl/ui/p_mooninfo.cpp
@@ -5,7 +5,7 @@ void fl::ui::mooninfo::update(PracticeUI &ui) {
 	StageScene *stageScene = ui.getStageScene();
 	ui.toggle("Hide Original Moon Counter", ui.hideShineCounter);
 
-#if (SMOVER == 100)
+#if SMOVER == 100
 	s32 currShine = GameDataFunction::getCurrentShineNum(*stageScene->mHolder);
 	s32 totalShine = GameDataFunction::getTotalShineNum(*stageScene->mHolder, -1);
 	s32 unlockShine = GameDataFunction::findUnlockShineNumByWorldId(0, *stageScene->mHolder, GameDataFunction::getCurrentWorldId(*stageScene->mHolder));
