613f700f43f57d676a8642c6eaa7c69892120ed0
FTSMO <100153792+FTSMO@users.noreply.github.com>
2022-07-06 15:27:16 -0600

    Added info about how to close the menu on 1.0
    Previously, the information on how to close the menu only applied to 1.3. I made it so that based on what version you compile to, it says something different

--- source/fl/ui/p_about.cpp
+++ source/fl/ui/p_about.cpp
@@ -2,7 +2,12 @@
 
 void fl::ui::about::update(PracticeUI &ui) {
 	ui.printf("Welcome to the Practice Mod!\n\n");
+#if SMOVER == 100
+	ui.printf("Press left stick to hide/unhide the menu.\n");
+#endif
+#if SMOVER == 130
 	ui.printf("Press L + left D-pad to hide/unhide the menu.\n");
+#endif
 	ui.printf("Press L + right D-pad to disable/enable the menu.\n\n");
 	ui.printf("Created by Fruityloops#8500 and contributors.\n");
 	ui.printf("Big thanks to CraftyBoss, bryce_____ and Mars2030!\n");
